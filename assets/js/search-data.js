b5d194d4d086a91986de26857ca3f82829e42cda
guniv <13756122+guniv@users.noreply.github.com>
2025-01-09 17:47:02 +0000

    Deploying to gh-pages from @ guniv/website@34b6a162d7b3d80ee53f80d2707633c628db1f07 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,20 +4,20 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/website/";
     },
   },{id: "nav-portfolio",
-          title: "portfolio",
+          title: "Portfolio",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/website/portfolio/";
           },
         },{id: "nav-résumé",
-          title: "résumé",
+          title: "Résumé",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
